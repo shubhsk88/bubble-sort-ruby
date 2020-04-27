65da809771adab5b82968c64ad1c3e572716d155
shubhsk88 <shubham88singh@gmail.com>
2020-04-28 03:49:12 +0530

    Finishing the normal sorting function

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,1 +1,17 @@
-a=2
+
+
+def bubble_sort(arr)
+    len=arr.length
+    for i in (0...len) do 
+        for j in (i+1...len) do
+            if(arr[i]>arr[j])
+                swap=arr[i]
+                arr[i]=arr[j]
+                arr[j]=swap
+            end
+        end
+    end
+ return arr;
+end 
+
+print bubble_sort([4,3,78,2,0,2])
